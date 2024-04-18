c0b347358e5fd7ae9aebe7fae825bd81bc289cf0
gabsantosmr <160122283+gabsantosmr@users.noreply.github.com>
2024-04-18 19:50:51 -0300

    Add files via upload

--- Aula7.java
+++ Aula7.java
@@ -3,19 +3,23 @@ package aulas;
 public class Aula7 {
 
 	public static void main(String[] args) {
+		// for
 		for (int i = 0; i < 10; i++) {
 			System.out.println(i);
 		}
+		// while
 		int a = 0;
 		while (a < 10) {
 			System.out.println(a);
 			a++;
 		}
+		// do while
 		int b = 0;
 		do {
 			System.out.println(b);
 			b++;
 		} while (b < 10);
+		// break
 		int c = 0;
 		while (c < 20) {
 		   if (c > 10) {
@@ -24,6 +28,7 @@ public class Aula7 {
 		   System.out.println(c);
 		   c++;
 		}
+		// continue
 		int d = 0;
 		while (d < 20) {
 		    if (d > 10) {
@@ -33,6 +38,7 @@ public class Aula7 {
 		    System.out.println(d);
 		    d++;
 		}
+		// referência
 		int e = 0;
 		externo: while (e < 20) {
 			  while (e > 10) {
