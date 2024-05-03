a4e263b195ec85afd5e907ef3ce656a952db797a
gabsantosmr <160122283+gabsantosmr@users.noreply.github.com>
2024-05-02 21:45:20 -0300

    Add files via upload

---
+++ Aula8.java
@@ -0,0 +1,15 @@
+package aulas;
+
+public class Aula8 {
+
+	public static void main(String[] args) {
+// Array
+		int[] valores = new int[3];
+		valores[0] = 0;
+		valores[1] = 1;
+		valores[2] = 2;
+		System.out.println(valores[0]);
+		System.out.println(valores[1]);
+		System.out.println(valores[2]);
+	}
+}
